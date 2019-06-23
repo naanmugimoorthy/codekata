7b432288dd71b9ed9031e6e6ebcfc0238fda7228
naanmugimoorthy <47532651+naanmugimoorthy@users.noreply.github.com>
2019-06-23 09:50:22 +0530

    Create vowel.c

---
+++ c/vowel.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  char ch;
+ 
+  printf("Enter a character\n");
+  scanf("%c", &ch);
+ 
+  // Checking both lower and upper case
+ 
+  if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch =='o' || ch=='O' || ch == 'u' || ch == 'U')
+    printf("%c is a vowel.\n", ch);
+  else
+    printf("%c isn't a vowel.\n", ch);
+     
+  return 0;
+  getch();
+  
+}
